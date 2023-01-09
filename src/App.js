c1cf96e11f896f47a107dffc0c798e252099a214
Mark Bieganek <mark.bieganek@telus.com>
2023-01-09 09:07:54 -0700

    Import button components, call in App() return

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 import './App.css';
 
 function App() {
   return (
-    null
+    <div className={`App`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
+    </div>
   );
 }
 
