68b0ce373084dadf29337df9cf53382935c9c981
Mark Bieganek <mark.bieganek@telus.com>
2023-01-09 09:26:00 -0700

    Set and use React state in components

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 function TextRepeaterButton() {
-  const [repetitions, setRepetitions] = useState();
+  const [repetitions, setRepetitions] = useState(1);
+  const textArray = Array.of(repetitions);
+  textArray.forEach((element, index) => {
+    textArray[index] = <span key={index}>I like this text</span>;
+  });
 
   return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+      {textArray}
   </button>
   );
 }
