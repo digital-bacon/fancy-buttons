6376319f914faa1bf6faeab63b84dcc7f6cb4362
Mark Bieganek <mark.bieganek@telus.com>
2023-01-09 10:55:58 -0700

    Refactor LightSwitchButton, receive state from App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
@@ -5,13 +6,15 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 import './App.css';
 
 function App() {
+  const [light, setLight] = useState('off');
+
   return (
     <div className={`App`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight} />
         <TextRepeaterButton />
       </section>
     </div>
